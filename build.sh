f9684d55b80189737d54f7a82132cfa5ec9b1320
Rob Schoening <robschoening@gmail.com>
2025-12-21 13:15:22 -0800

    update

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 set -e 
 
 
-if [[ "${CI}" = "true" ]]; then
-env
-    cat <<EOF >./config.yml
-MASSIVE_API_KEY: ${MASSIVE_API_KEY}
-EOF
-fi
+#if [[ "${CI}" = "true" ]]; then
+#
+#    cat <<EOF >./config.yml
+#MASSIVE_API_KEY: ${MASSIVE_API_KEY}
+#EOF
+#fi
 
 ./mvnw -B clean test 
 
