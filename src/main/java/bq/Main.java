f624538e4cba6c62a9d594729172108ca472959c
Rob Schoening <robschoening@gmail.com>
2025-12-26 20:42:33 -0800

    remove old code

--- src/main/java/bq/Main.java
+++ src/main/java/bq/Main.java
@@ -1,18 +1,11 @@
 package bq;
 
 import bx.util.Slogger;
-import java.util.Map;
-import jdk.jshell.JShellConsole;
 import org.slf4j.Logger;
 
 public class Main {
 
   static Logger logger = Slogger.forEnclosingClass();
 
-  static ThreadLocal<Map<String, Object>> threadLocal = new ThreadLocal<Map<String, Object>>();
-
-  public static void main(String[] args) throws Exception {
-
-    JShellConsole x;
-  }
+  public static void main(String[] args) throws Exception {}
 }
